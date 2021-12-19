3cd2d74282ce7bd0e032c6b3e5d17488775544b3
KK-kk056 <Khalid.Alyami.eam.js@tuwaiq.edu.sa>
2021-12-19 11:52:29 +0300

    update

--- Models/UserSchema.js
+++ Models/UserSchema.js
@@ -4,8 +4,8 @@ const UserSchema = new Schema({
     nationalid:{ type:String , required: true},
     fullName:{ type:String , required: true},
     password:{ type:String , required: true},
-    role: { type:String , required: true}
+    role:  {type:mongoose.Schema.ObjectId, ref:"Roles"}
 })
 
-const User= module.model('User', User)
+const User= module.model('User', UserSchema)
 module.exports= User;
